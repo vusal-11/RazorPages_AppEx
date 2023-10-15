99256355cc6340486202ff647d3aaa2311530580
Vusal Aliyev <vusal.aliyev7472@gmail.com>
2023-10-15 14:51:37 +0400

    initial commit

---
+++ RazorPages_AppEx/Data/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace RazorPages_AppEx.Data
+{
+    public class Person
+    {
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+    }
+}
---
+++ RazorPages_AppEx/Pages/Index.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPages_AppEx.Data;
+using System.Text;
+
+namespace RazorPages_AppEx.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        //public IndexModel()
+        //{
+        //}
+
+
+        public string foo(string name)
+        {
+            return $"{name} foo";
+        }
+        //public void OnGet(string name,int age)
+        //{
+
+        //    ViewData["Name"]=name;
+        //    ViewData["Age"]=age;
+
+
+        //}
+
+
+        //public void OnGet(Person person)
+        //{
+
+        //    ViewData["Name"] = person.Name;
+        //    ViewData["Age"] = person.Age;
+
+
+        //}
+
+
+        //public void OnGet(string[] names)
+        //{
+
+        //    StringBuilder message = new StringBuilder();
+        //    foreach (string name in names)
+        //    {
+        //        message.Append($"{name} ");
+        //    }
+        //    ViewData["Name"] = message.ToString();
+        //    ViewData["Age"] = 0;
+
+        //}
+
+
+        //public ContentResult OnGet()
+        //{
+        //    return Content("My content text");
+        //}
+
+
+        private readonly IWebHostEnvironment _environment;
+
+        public IndexModel(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public FileResult OnGet()
+        {
+            string path = Path.Combine(_environment.ContentRootPath, "Data/file.txt");
+            var bytes =System.IO.File.ReadAllBytes(path);
+            return File(bytes, "Application/text", "Salam.txt");
+        }
+
+
+
+    }
+}
